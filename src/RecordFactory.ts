fd7a23592282f5623a17a6aa820ace6e1e6294c7
Anatolii Nedelkin <nedelkin.anat@gmail.com>
2017-02-28 22:03:15 +0300

    add Comment

--- src/RecordFactory.ts
+++ src/RecordFactory.ts
@@ -2,11 +2,13 @@ import {RecordType} from "./enums/record-type";
 import {Header} from "./reсords/Header";
 import {SetMapMode} from "./reсords/SetMapMode";
 import {ModifyWorldTranform} from "./reсords/ModifyWorldTranform";
+import {Comment} from "./reсords/Comment";
 
 let records = {
 	[RecordType.EMR_HEADER]: Header,
 	[RecordType.EMR_SETMAPMODE]: SetMapMode,
-	[RecordType.EMR_MODIFYWORLDTRANSFORM]: ModifyWorldTranform
+	[RecordType.EMR_MODIFYWORLDTRANSFORM]: ModifyWorldTranform,
+	[RecordType.EMR_COMMENT]: Comment
 };
 
 export class RecordFactory {
---
+++ src/reсords/Comment.ts
@@ -0,0 +1,12 @@
+export class Comment {
+	constructor() {
+		console.log('Comment');
+	}
+
+	private structure = {
+		Type: 32,
+		Size: 32,
+		DataSize: 32,
+		PrivateData: "variable"
+	};
+}
